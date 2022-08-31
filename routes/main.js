09e56bd0d907edce7802fef2e553c7a1528769fc
mackawara <mkawara@outlook.com>
2022-08-31 18:15:19 +0200

    feat : removed listing

--- routes/main.js
+++ routes/main.js
@@ -3,14 +3,15 @@ const router = express.Router()
 const authController = require('../controllers/auth') 
 const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
-const listingsController=require("../controllers/listings")
+
 
 //router.get('/', homeController.getHome)
-router.get('/listings', homeController.getListings)
+
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
+
 
 module.exports = router
