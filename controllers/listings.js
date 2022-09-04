6d5478501d2cc5e1f967843b36cd3007b354df40
mackawara <mkawara@outlook.com>
2022-09-04 21:03:33 +0200

    feat: added search controller

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,34 +1,34 @@
 const saveListing = require("../models/listings");
 const ListingModel = require("../models/listings");
 
-exports.getListings = async (req, res) => {
+(exports.getListings = async (req, res) => {
   console.log(req.user);
   try {
     const listings = await ListingModel.find();
-console.log(listings)
-    res.render("listings.ejs", { listings: listings });
+    console.log(listings);
+    res.render("listings/listings.ejs", { listings: listings });
   } catch (err) {
     console.log(err);
   }
-},
+}),
   (exports.getAddListings = async (req, res) => {
     console.log(" req receieved");
-    res.render("addListing.ejs");
+    res.render("listings/addListing.ejs");
   }),
   (exports.addListing = async (req, res, next) => {
     const body = req.body;
-    console.log(req.body.range);
+    console.log(req.body.rooms);
     console.log(req.body.location);
-    console.log(req.body.description);
+    // console.log(req.body.description);
     console.log(req.body.city);
 
     const newListing = new ListingModel({
       location: body.location,
-    range: body.range,
-    rooms: body.rooms,
-    description: body.description,
-    city: body.city,
-    range: body.range,
+      range: body.range,
+      rooms: body.rooms,
+      description: body.description,
+      city: body.city,
+      range: body.range,
       /* location: body.description,
       fullAddress: body.fullAddress,
       pic1: body.fullAddress,
@@ -39,8 +39,8 @@ console.log(listings)
     const searchExistingListing = async function () {
       console.log(`searchExisting listing is working`);
       const result = await ListingModel.find({
-        descripton: body.descripton,
-      location: body.location,
+        description: body.description,
+        location: body.location,
       }).exec();
       console.log(result);
 
@@ -66,5 +66,14 @@ console.log(listings)
     }
   }),
   (exports.searchListings = async (req, res) => {
-    res.render("searchListing.ejs");
+    const listings =await ListingModel.find({
+      location: req.body.location,
+      city: req.body.city,
+    });
+    console.log(listings)
+    console.log(req.body.location, req.body.city)
+    res.render("listings/listings.ejs", { listings: listings });
+  }),
+  (exports.getSearchListings = async (req, res) => {
+    res.render("listings/search.ejs");
   });
