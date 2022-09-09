f885f3e6b16ccc3f237c992e9777d65726c96316
mackawara <mkawara@outlook.com>
2022-09-09 22:31:25 +0200

    changes to routes

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
-
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth");
+const homeController = require("../controllers/home");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //router.get('/', homeController.getHome)
-router.get('/home', homeController.getIndex)
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
+router.get("/home", homeController.getIndex);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
-
-module.exports = router
+module.exports = router;
