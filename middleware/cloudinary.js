88255dbd19b164094e3e7b300894d9c7d02b68a5
mackawara <mkawara@outlook.com>
2022-09-29 18:24:44 +0200

    changed cloudinary config

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -3,9 +3,10 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({ 
-  cloud_name: 'df94bonis', 
-  api_key: '496535993249397', 
-  api_secret: '2EN5RDxm_Xu7X4h7SkcEdgSozG8' 
+  cloud_name:process.env.CLOUDINARY_NAME , 
+  api_key:process.env.CLOUDINARY_KEY , 
+  api_secret:process.env.CLOUDINARY_SECRET  
 });
+
 
 module.exports = cloudinary;
